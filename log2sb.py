afd79b779b2e49ba91530634ea049629555ec9fd
Ayan Chakrabarti <ayan@wustl.edu>
2020-11-19 16:59:43 -0600

    .

--- log2sb.py
+++ log2sb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # - Ayan Chakrabarti <ayan.chakrabarti@gmail.com>
-"""Convert log files to html."""
+"""Convert log files to interactive html plot."""
 
 import json
 import sys
@@ -8,95 +8,109 @@ import re
 import numpy as np
 
 HOSTURL = 'https://ayanc.github.io/sb/assets/'
-
-# Code to output json
-html_pfx='''
-<html><head><link rel="stylesheet" type="text/css" href="HOSTURLsb.css">
+HTML_DATA = [
+    '''
+<html><head>
+<link rel="stylesheet" type="text/css" href="HOSTURLsb.css">
 <script src="https://cdn.plot.ly/plotly-1.31.2.min.js"></script>
 <script src="HOSTURLsb.js"></script>
 <script type="text/javascript">
-'''.replace('HOSTURL', HOSTURL).strip('\n')
-
-html_sfx='''
-</script></head><body></body></html>
-'''.strip('\n')
+'''.replace('HOSTURL', HOSTURL).strip('\n'),
+    '</script></head><body></body></html>']
 
 
-def smooth(x, y, xmax):
+def smooth(xdata, ydata, xmax, _limit=int(5e2)):
     """Simple fast smoother."""
-    LIM=int(5e2)
-    xmax = len(x)*xmax/x[-1]
-    if xmax <= 2*LIM:
-        return list(x), list(y)
+    xmax = len(xdata)*xmax/xdata[-1]
+    if xmax <= 2*_limit:
+        return xdata, ydata
 
-    fac = int(np.floor(xmax/LIM))
-    pad = (-len(x)) % fac
-    x = np.pad(x, [[pad, 0]], 'edge')
-    y = np.pad(y, [[pad, 0]], 'edge')
-    x = np.mean(np.reshape(x, (-1, fac)),1)
-    y = np.mean(np.reshape(y, (-1, fac)),1)
+    fac = int(np.floor(xmax/_limit))
+    pad = (-len(xdata)) % fac
+    xdata = np.pad(xdata, [[pad, 0]], 'edge')
+    ydata = np.pad(ydata, [[pad, 0]], 'edge')
+    xdata = np.mean(np.reshape(xdata, (-1, fac)), 1)
+    ydata = np.mean(np.reshape(ydata, (-1, fac)), 1)
 
-    if len(y) < 3:
-        return list(x), list(y)
-    return list(x), [y[0]] + list(
-        0.5*y[1:-1] + 0.25*y[:-2] + 0.25*y[2:]
-    ) + [y[-1]]
+    if len(ydata) >= 3:
+        ydata[1:-1] = 0.5*ydata[1:-1] + 0.25*ydata[:-2] + 0.25*ydata[2:]
+    return xdata, ydata
 
 
-def write_data(out,exps,tags,data):
+def write_data(out, exps, tags, data):
+    """Write data to out."""
+    fmtr = {'float_kind': lambda x: '%.4e' % x}
     dstr = []
     xmax = 0
-    for i in range(len(data)):
-        xmax = xmax if data[i][0][-1] < xmax else data[i][0][-1]
-    for i in range(len(data)):
-        steps, vals = smooth(data[i][0], data[i][1], xmax)
-        steps = ','.join(['%.4e' % float(i) for i in steps])
-        vals = ','.join(['%.4e' % float(i) for i in vals])
-        dstr.append("'" + data[i][2]
-                    + "': { x: [" + steps + "], y: ["
-                    + vals + "]}")
+    for _d in data:
+        xmax = xmax if _d[0][-1] < xmax else _d[0][-1]
 
-    out.write(html_pfx)
-    out.write('exps=' + json.dumps(exps) + ';')
-    out.write('tags=' + json.dumps(tags) + ';')
-    out.write("data= {" + ",".join(dstr) + "};");
-    out.write(html_sfx)
-##########################
+    for _d in data:
+        steps, vals = smooth(_d[0], _d[1], xmax)
+        # steps = ','.join(['%.4e' % float(j) for j in steps])
+        # vals = ','.join(['%.4e' % float(j) for j in vals])
+        steps = np.array2string(steps, separator=',', formatter=fmtr)
+        vals = np.array2string(vals, separator=',', formatter=fmtr)
+        dstr.append("\n'" + _d[2] + "': {\n x: " + steps
+                    + ",\n y: " + vals + "}")
 
-# Parse command line args
-if len(sys.argv) == 1:
-    sys.exit("USAGE: log2sb.py ID:/path/to/log/file [ID2:/path/to/log/file/2] ...")
-v = sys.argv[1:]
-files = [f.split(':')[-1] for f in v]
-exps = [v[i].split(':')[0] if len(v[i].split(":")) == 2 else str(i) for i in range(len(v))]
+    out.write(HTML_DATA[0])
+    out.write('exps=' + json.dumps(exps) + ';\n')
+    out.write('tags=' + json.dumps(tags) + ';\n')
+    out.write("data= {" + ",".join(dstr) + "};")
+    out.write(HTML_DATA[1])
 
 
-# Parse log files
-plots, tags = [], {}
-for j in range(len(files)):
-    try:
-        lines = re.sub('[\ ]+',' ',re.sub('[,=]',' ',open(files[j]).read()))
-    except:
-        continue
-    lines = [[q[0]]+q[1].split(' ') for q in re.findall(r'\[(\d+)\] ([^\n]*)\n',lines)]
+def parseargs():
+    """Parse command line arguments"""
+    if len(sys.argv) == 1:
+        sys.exit("USAGE: log2sb.py ID:/path/to/log/file " +
+                 "[ID2:/path/to/log/file/2] ...")
+    args = sys.argv[1:]
+    files = [f.split(':')[-1] for f in args]
+    exps = [f.split(':')[0] if len(f.split(":")) == 2
+            else str(i) for i, f in enumerate(args)]
+    return files, exps
 
-    data = {}
-    for l in lines:
-        it = int(l[0])
-        for k in range(1,len(l)-1,2):
-            tag = l[k]
-            if tag not in data.keys():
-                data[tag] = [[],[]]
 
-            data[tag][0].append(it)
-            data[tag][1].append(float(l[k+1]))
-            
-    for tag in data.keys():
-        tags[tag] = 1
-        its = np.float32(data[tag][0])
-        vals = np.float32(data[tag][1])
-        nm = tag + '@' + exps[j]
-        plots.append([its,vals,nm])
+def getdata(files, exps):
+    """Parse log files"""
+    plots, tags = [], set()
+    for fnm, exp in zip(files, exps):
+        try:
+            lines = re.sub(r'[\ ,=]+', ' ', open(fnm).read())
+        except:
+            continue
 
-# Write        
-write_data(sys.stdout,exps,sorted(list(tags.keys())),plots)
+        lines = [[q[0]] + q[1].split(' ') for
+                 q in re.findall(r'\[(\d+)\] ([^\n]*)\n', lines)]
+
+        data = {}
+        for _l in lines:
+            itrs = _l[0]
+            for k in range(1, len(_l), 2):
+                tag, val = _l[k], _l[k+1]
+                if tag not in data.keys():
+                    data[tag] = [[itrs], [val]]
+                else:
+                    data[tag][0].append(itrs)
+                    data[tag][1].append(val)
+
+        tags |= set(data.keys())
+        for tag in data:
+            its = np.float32(data[tag][0])
+            vals = np.float32(data[tag][1])
+            name = tag + '@' + exp
+            plots.append([its, vals, name])
+    return plots, list(tags)
+
+
+def main():
+    """Main function"""
+    files, exps = parseargs()
+    plots, tags = getdata(files, exps)
+    write_data(sys.stdout, exps, tags, plots)
+
+
+if __name__ == "__main__":
+    main()
