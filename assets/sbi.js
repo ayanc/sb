acbfd3dea069251ef2e79cf67c876f592bfe5fd7
Ayan Chakrabarti <ayan@wustl.edu>
2020-11-22 10:49:05 -0600

    .

---
+++ assets/sbi.js
@@ -0,0 +1,122 @@
+// Ayan Chakrabarti <ayan.chakrabarti@gmail.com>
+
+showIndiv = function() {
+    group = parseInt(document.querySelector('#group').value);
+    exp = parseInt(document.querySelector('#exp').value);
+    indiv = parseInt(document.querySelector('#indiv').value);
+
+    imgs = data[group][3][exp][1][indiv];
+    for(i = 0; i < imgs.length; i++) {
+        document.querySelector("#lnk"+i).setAttribute("href", imgs[i]);
+        document.querySelector("#img"+i).setAttribute("src", imgs[i]);
+    }
+    history.replaceState(undefined, undefined, "#"+group+"#"+exp+"#"+indiv);
+}
+
+iswitch = function(delta) {
+    group = parseInt(document.querySelector('#group').value);
+    exp = parseInt(document.querySelector('#exp').value);
+    indiv = parseInt(document.querySelector('#indiv').value);
+
+    indiv = indiv + delta;
+    if(indiv < 0) indiv = 0;
+    if(indiv >= data[group][2].length) indiv = data[group][2].length-1;
+    document.querySelector('#indiv').selectedIndex = indiv;
+    showIndiv();
+}
+
+eswitch = function(delta) {
+    group = parseInt(document.querySelector('#group').value);
+    exp = parseInt(document.querySelector('#exp').value);
+    indiv = parseInt(document.querySelector('#indiv').value);
+
+    exp = exp  + delta;
+    if(exp < 0) exp = 0;
+    if(exp >= data[group][3].length) exp = data[group][3].length-1;
+    document.querySelector('#exp').selectedIndex = exp;
+    showIndiv();
+}
+
+showGroup = function(val, exp, indiv) {
+    html = ''; sublist = data[val][3];
+    for(i = 0; i < sublist.length; i++) {
+        if(i == exp)
+            html = html + '<option selected="selected" value='+i+'>'+sublist[i][0]+'</option>';
+        else
+            html = html + '<option value='+i+'>'+sublist[i][0]+'</option>';
+    }
+    document.querySelector('#exp').innerHTML = html;
+
+    html = ''; inlist = data[val][2];
+    for(i = 0; i < inlist.length; i++) {
+        if(i == indiv)
+            html = html + '<option selected="selected" value='+i+'>'+inlist[i]+'</option>';
+        else
+            html = html + '<option value='+i+'>'+inlist[i]+'</option>';
+    }
+    document.querySelector('#indiv').innerHTML = html;
+
+    html = ''; columns = data[val][1];
+    for(i = 0; i < columns.length; i++)
+        html = html + '<div class="column"><a id="lnk'+i+'" target=_blank><img id="img'+i+'"></a><br/>'+columns[i]+'</div>';
+    document.querySelector('#gallery').innerHTML = html;
+
+    showIndiv();
+}
+
+groupChg = function(val) {
+    val = parseInt(val);
+    showGroup(parseInt(val), 0, 0);
+}
+
+onKey = function(e) {
+    if(e.keyCode == 106) {iswitch(1); return false;}
+    if(e.keyCode == 107) {iswitch(-1); return false;}
+
+    if(e.keyCode == 104) {eswitch(-1); return false;}
+    if(e.keyCode == 108) {eswitch(1); return false;}
+}
+
+window.onload = function() {
+    document.body.innerHTML = `
+        <div class="head">
+            <div>
+                <table><tr style="vertical-align: center;">
+                    <td id="title">StaticBoard</td>
+                    <td>Group: <select style="width: 250px;" id='group' onclick='javascript:groupChg(this.value);' onchange='javascript:groupChg(this.value);'></select></td>
+                    <td>Exp: <select style="width: 250px;" id='exp' onclick='javascript:showIndiv();' onchange='javascript:showIndiv();'></select></td>
+                </tr></table>
+            </div>
+        </div>
+        <div class="graph">
+            <div id="selector">
+                <button onclick="javascript:iswitch(-1);">Prev</button>
+                <select style="width: 250px;" id='indiv' onclick='javascript:showIndiv();' onchange='javascript:showIndiv();'></select>
+                <button onclick="javascript:iswitch(1);">Next</button>
+            </div>
+            <div id="gallery"></div>
+        </div>
+`;
+
+    // Setup list of groups
+    hash = window.location.hash.split('#');
+    if(hash.length < 4) {
+        selgroup = 0; selexp = 0; selind = 0;
+    } else {
+        selgroup = parseInt(hash[1]);
+        selexp = parseInt(hash[2]);
+        selind = parseInt(hash[3]);
+    }
+
+    html = '';
+    for(i = 0; i < data.length; i++) {
+        if(i == selgroup)
+            html = html + '<option selected="selected" value='+i+'>'+data[i][0]+'</option>';
+        else
+            html = html + '<option value='+i+'>'+data[i][0]+'</option>';
+    }
+    document.querySelector('#group').innerHTML = html;
+    showGroup(selgroup, selexp, selind);
+
+    document.onkeypress = onKey;
+};
---
+++ img2sb.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+"""Convert YAML to html."""
+
+import sys
+import re
+import itertools
+import json
+from glob import glob
+import yaml
+
+
+HOSTURL = 'https://ayanc.github.io/sb/assets/'
+HTML_DATA = [
+    '''
+<html><head>
+<link rel="stylesheet" type="text/css" href="HOSTURLsbi.css">
+<script src="HOSTURLsbi.js"></script>
+<script type="text/javascript">
+'''.replace('HOSTURL', HOSTURL).strip('\n'),
+    '</script></head><body></body></html>']
+
+
+def parse(fname):
+    """Convert from YAML + glob to json."""
+    flist = glob('**', recursive=True)
+
+    config = yaml.load(open(fname).read(), Loader=yaml.CLoader)
+    outs = []
+
+    # Handle each block
+    for exp in config:
+
+        # Parse block description, match files
+        ename, econfig = exp.popitem()
+        keys = econfig[0].split('+')
+        kvals = {k: None for k in keys}
+        cnames, cfiles, ckeys = [], [], []
+        for idx in range(1, len(econfig)):
+            cname, creg_ = econfig[idx].popitem()
+            creg_ = creg_.replace('(', '(?P<').replace(')', r'>[^/\\]*)')
+            creg = re.compile(creg_)
+            cfile_ = [(p[0], p.groupdict()) for p in map(creg.match, flist)
+                      if p is not None]
+            if len(cfile_) == 0:
+                sys.stderr.write("Could not find match to: %s\n" % creg_)
+                sys.exit(128)
+            ckey = list(cfile_[0][1].keys())
+            ckvals = {k: set() for k in ckey}
+            cfile = {}
+            for _f in cfile_:
+                cfile[':'.join(_f[1].values())] = _f[0]
+                for _k in ckey:
+                    ckvals[_k].add(_f[1][_k])
+
+            for _k in ckey:
+                if kvals[_k] is None:
+                    kvals[_k] = ckvals[_k]
+                else:
+                    kvals[_k] = kvals[_k].intersection(ckvals[_k])
+
+            cnames.append(cname)
+            cfiles.append(cfile)
+            ckeys.append(ckey)
+
+        # Convert to lists of arrays
+        labels = [sorted(list(kvals[k])) for k in keys]
+        subexps = list(itertools.product(*labels[:-1]))
+        sublist = []
+        for sub in subexps:
+            sdict = {keys[k]: sub[k] for k in range(len(sub))}
+            jname = ', '.join(['%s=%s' % (k, sdict[k]) for k in sdict])
+            rows = []
+            for lastkey in labels[-1]:
+                sdict[keys[-1]] = lastkey
+                row = []
+                for _j, ckey in enumerate(ckeys):
+                    col = cfiles[_j][':'.join([sdict[k] for k in ckey])]
+                    row.append(col)
+                rows.append(row)
+            sublist.append([jname, rows])
+
+        group = [ename, cnames, labels[-1], sublist]
+        outs.append(group)
+
+    return 'data='+json.dumps(outs)+';'
+
+
+def getargs():
+    """Get yaml file name or print usage."""
+    if len(sys.argv) < 2:
+        sys.exit("USAGE: img2sb.py yaml-file-name")
+    return sys.argv[1]
+
+
+def main():
+    """Main function"""
+    data = parse(getargs())
+    sys.stdout.write(HTML_DATA[0] + data + HTML_DATA[1])
+
+
+if __name__ == "__main__":
+    main()
